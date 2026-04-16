17b844b15f60dc630475ddac08c9806a1e48702b
2014000061-JosePortillo <j.portillo.5adb@gmail.com>
2026-04-15 18:59:44 -0600

    Cuadro 5 de la GA3

---
+++ SOL_IVD_JosePortillo23/src/main/java/com/mycompany/sol_ivd_joseportillo23/SOL_IVD_JosePortillo23.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sol_ivd_joseportillo23;
+
+/**
+ *
+ * @author VAN ALLEN
+ */
+public class SOL_IVD_JosePortillo23 {
+
+    public static void main(String[] args) {
+        jfrmMensajes M = new jfrmMensajes();
+        M.setVisible(true);
+    }
+}
